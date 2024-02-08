3ecea225a24a33c5e78a9b46f88e3dd1d3b97b6a
matus42 <134312272+matus42@users.noreply.github.com>
2024-02-08 21:58:57 +0000

    Feature: Prevent past date selection in booking form

--- booking/forms.py
+++ booking/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Booking
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 
 class BookingForm(forms.ModelForm):
@@ -10,7 +12,7 @@ class BookingForm(forms.ModelForm):
         fields = ['massage', 'date', 'time_slot']
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'),
-        }    
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,3 +20,10 @@ class BookingForm(forms.ModelForm):
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Book Now'))
         self.fields['date'].input_formats = ('%Y-%m-%d',)
+
+    def clean_date(self):
+        date = self.cleaned_data['date']
+        today = timezone.localdate()
+        if date < today:
+            raise ValidationError("Booking for past dates is not allowed.")
+        return date

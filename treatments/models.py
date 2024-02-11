60f42576768dcae73d48ed2c3390297333273b6c
matus42 <134312272+matus42@users.noreply.github.com>
2024-02-11 19:53:39 +0000

    Add Cloudinary for media storage management

--- treatments/models.py
+++ treatments/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
 class Massage(models.Model):
     name = models.CharField(max_length=100)
+    featured_image = CloudinaryField('image', default='placeholder')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     duration = models.IntegerField(help_text="Duration in minutes")

189e90792b22e5966174307d3c178dd2ed7eb3fa
matus42 <134312272+matus42@users.noreply.github.com>
2024-02-13 11:27:52 +0000

    Add favicon

--- treatments/urls.py
+++ treatments/urls.py
@@ -3,5 +3,7 @@ from .views import home, MassageDetailView
 
 urlpatterns = [
     path('', home, name='home'),
-    path('massages/<int:pk>/', MassageDetailView.as_view(), name='massage_detail'),
+    path('massages/<int:pk>/',
+         MassageDetailView.as_view(),
+         name='massage_detail'),
 ]
--- treatments/views.py
+++ treatments/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 from .models import Massage
 from django.views.generic import DetailView
 
-# Create your views here.
-
 
 def home(request):
+    """
+    Render the homepage with a list of massages.
+    """
     massages = Massage.objects.all()
     return render(request, 'treatments/home.html', {'massages': massages})
 
 
 class MassageDetailView(DetailView):
+    """
+    Display detailed information for a specific massage.
+    """
     model = Massage
     template_name = 'treatments/massage_detail.html'
     context_object_name = 'massage'
